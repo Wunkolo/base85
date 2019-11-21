e20e19d7d97757e44e48e9fae9fef7b1d2a67548
Wunkolo <Wunkolo@gmail.com>
2019-11-21 01:02:45 -0800

    Optimize base85 encoding
    Formatted this way allows for InTuple to not be relied upon between loops.
    Allows for some vectorization oppritunities.

--- source/Base85.cpp
+++ source/Base85.cpp
@@ -9,16 +9,19 @@ void Base85::Encode(
 	const std::uint8_t Input[], std::size_t Length, std::uint8_t Output[]
 )
 {
+	const std::uint32_t Pow85[5] = {
+		52200625ul, 614125ul, 7225ul, 85ul, 1ul
+	};
 	for( std::size_t i = 0; i < Length / 4; ++i )
 	{
-		std::uint32_t InTuple = __builtin_bswap32(
+		const std::uint32_t InTuple = __builtin_bswap32(
 			*reinterpret_cast<const std::uint32_t*>(&Input[i * 4])
 		);
-		for( std::size_t j = 0; j < 5; ++j )
-		{
-			Output[i * 5 + (4u - j)] = InTuple % 85 + '!';
-			InTuple /= 85;
-		}
+		Output[i * 5 + 0] = ((InTuple / Pow85[0]) % 85ul) + '!';
+		Output[i * 5 + 1] = ((InTuple / Pow85[1]) % 85ul) + '!';
+		Output[i * 5 + 2] = ((InTuple / Pow85[2]) % 85ul) + '!';
+		Output[i * 5 + 3] = ((InTuple / Pow85[3]) % 85ul) + '!';
+		Output[i * 5 + 4] = ((InTuple / Pow85[4]) % 85ul) + '!';
 	}
 }
 
@@ -26,11 +29,11 @@ void Base85::Decode(
 	const std::uint8_t Input[], std::size_t Length, std::uint8_t Output[]
 )
 {
+	const std::uint32_t Pow85[5] = {
+		52200625ul, 614125ul, 7225ul, 85ul, 1ul
+	};
 	for( std::size_t i = 0; i < Length / 5; ++i )
 	{
-		const std::uint32_t Pow85[5] = {
-			52200625ul, 614125ul, 7225ul, 85ul, 1ul
-		};
 		std::uint32_t& OutTuple = *reinterpret_cast<std::uint32_t*>(&Output[i * 4]);
 		OutTuple = 0;
 		for( std::size_t j = 0; j < 5; ++j )
